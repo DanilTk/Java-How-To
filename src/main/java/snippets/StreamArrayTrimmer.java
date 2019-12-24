e25c29904976ee36158e316122b6d78a53da864d
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-12-24 18:58:36 +0100

    StreamArrayTrimmer updated

--- src/main/java/snippets/StreamArrayTrimmer.java
+++ src/main/java/snippets/StreamArrayTrimmer.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
- * Code snippet shows how to trim 2d array with stream and save back to 2d array
+ * Code snippet shows how to trim 2d array with stream and save back to 2d array.
+ * Results 1 and 2 are the same and limit size of sub-array, but result 3 is dynamical
  */
 
 public class StreamArrayTrimmer {
@@ -12,7 +13,7 @@ public class StreamArrayTrimmer {
     public static void streamAndTrimArray() {
         String[][] arr = {{"  word  ", " word  "}, {"  word  ", " word  "}, {"  word  ", " word  ", "word  "}};
 
-        String[][] result = IntStream.range(0, arr.length)
+        String[][] result1 = IntStream.range(0, arr.length)
                 .mapToObj(i -> IntStream.range(0, arr[0].length)
                         .mapToObj(j -> arr[i][j].trim())
                         .toArray(String[]::new))
@@ -23,6 +24,12 @@ public class StreamArrayTrimmer {
                         .mapToObj(j -> strings[j].trim())
                         .toArray(value1 -> new String[value1]))
                 .toArray(value -> new String[value][]);
+
+        String[][] result3 = Arrays.stream(arr)
+                .map(strings -> Arrays.stream(strings)
+                        .map(String::trim)
+                        .toArray(String[]::new))
+                .toArray(String[][]::new);
 
     }
 }
