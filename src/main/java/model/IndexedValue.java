b0e1450c8c3aa3d07731f9a22977f3520a6eb2a3
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2020-11-07 09:37:42 +0100

    Cleanups

--- src/main/java/model/IndexedValue.java
+++ src/main/java/model/IndexedValue.java
@@ -3,8 +3,8 @@ package model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
 @Data
+@AllArgsConstructor
 public class IndexedValue<T> {
     Integer index;
     T value;
